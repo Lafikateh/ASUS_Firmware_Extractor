2045a87834afa4d6c5fbdc08f53b7be651c3adf6
Lafikateh <73808989+Lafikateh@users.noreply.github.com>
2021-07-04 23:04:55 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,18 +4,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Program Headers
-#include "Main.h"
-#include "Files.h"
-#include "Strings.h"
-
 #define ASUS_AMI_CAP_Header_Size 2048
+
+bool read_file(FILE* handle, void* buffer, unsigned int* size)
+{
+	if(handle != NULL && size != NULL)
+	{
+		if(buffer != NULL)
+		{
+			fseek(handle, 0, SEEK_END);
+			*size = ftell(handle);
+			fseek(handle, 0, SEEK_SET);
+			fread(buffer, 1, *size, handle);
+		}
+		else
+		{
+			fseek(handle, 0, SEEK_END);
+			*size = ftell(handle);
+			fseek(handle, 0, SEEK_SET);
+		}
+		
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
 
 // Program Entry Point
 int main(int argument_count, char* argument_list[])
 {
 	// Print program header
-	printf("ASUS UEFI Firmware Extractor v1.0.1\n");
+	printf("ASUS UEFI Firmware Extractor v1.0.2\n");
 
 	if (argument_count == 1)
 	{
@@ -36,47 +57,52 @@ int main(int argument_count, char* argument_list[])
 		}
 		else
 		{
-			target_path = argument_list[1];
-			target_path = c_replace_string_c(target_path, ".CAP", ".rom");
-			target_path = c_replace_string_c(target_path, ".cap", ".rom");
+			target_path = strdup(argument_list[1]);
+			int length = strlen(target_path);
+			
+			if(strcmp(target_path + (length - 3), "CAP") == 0)
+			{
+				strcpy(target_path + (length - 3), "rom");
+			}
+			else if(strcmp(target_path + (length - 3), "cap") == 0)
+			{
+				strcpy(target_path + (length - 3), "rom");
+			}
 		}
 
-		// Check if the source file exists
-		if (check_if_exists(source_path) == true)
+		// Attempt to open the source file
+		FILE* source_file = fopen(source_path, "rb");
+		if(source_file != NULL)
 		{
-			// Attempt to read the source file
-			data_buffer* source_buffer = read_file(source_path);
-			if (source_buffer != NULL)
+			// Get the source file size
+			unsigned int source_file_size = 0;
+			read_file(source_file, NULL, &source_file_size);
+			
+			// Read the source file
+			unsigned char* source_file_buffer = malloc(source_file_size);
+			bool read_result = read_file(source_file, source_file_buffer, &source_file_size);
+			if (read_result == true)
 			{
 				// Allocate and zero-fill the target data buffer
-				unsigned long target_data_buffer_size = source_buffer->size - ASUS_AMI_CAP_Header_Size;
-				unsigned char* target_data_buffer = calloc(target_data_buffer_size, sizeof(unsigned char));
+				unsigned long target_file_size = source_file_size - ASUS_AMI_CAP_Header_Size;
+				unsigned char* target_file_buffer = malloc(target_file_size);
 
 				// Copy the UEFI Image to the target file buffer
-				memcpy(target_data_buffer, source_buffer->data + ASUS_AMI_CAP_Header_Size, target_data_buffer_size);
+				memcpy(target_file_buffer, source_file_buffer + ASUS_AMI_CAP_Header_Size, target_file_size);
 
-				// Create the target file buffer
-				data_buffer* target_buffer = create_buffer(target_data_buffer, target_data_buffer_size);
-
-				// Attempt to write the modified file
-				bool write_result = write_file(target_path, target_buffer);
+				// Write the modified file
+				FILE* destination_file = fopen(target_path, "wb");
+				fwrite(target_file_buffer, 1, target_file_size, destination_file);
 
 				// Free the file buffers
-				free_buffer(target_buffer);
-				free_buffer(source_buffer);
+				free(target_file_buffer);
+				free(source_file_buffer);
+				
+				// Close the file handles
+				fclose(destination_file);
+				fclose(source_file);
 
-				if (write_result == true)
-				{
-					return 0;
-				}
-				else
-				{
-					// Print error
-					printf("Failed to write file \"%s\"", target_path);
-
-					// Return failure
-					return 1;
-				}
+				return 0;
 			}
 			else
 			{
